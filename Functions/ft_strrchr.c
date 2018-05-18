05aafef219796acf13b6a121c3de9f4350f490b8
Meghan WESTVIG <mwestvig@c2r3s5.wethinkcode.co.za>
2018-05-18 13:26:35 +0200

    Added strrchr

---
+++ Functions/ft_strrchr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mwestvig <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/18 13:16:39 by mwestvig          #+#    #+#             */
+/*   Updated: 2018/05/18 13:25:03 by mwestvig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <string.h>
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char ch;
+	char *ptr;
+	int i;
+
+	i = 0;
+	ch = (int)(c + '0');
+	ptr = (char*)s;
+	while (*ptr++)
+		i++;
+	if (*ptr == c)
+		return (ptr);
+	while (i-- > 0)
+	{
+		if (*ptr == c)
+			return (ptr);
+		ptr--;
+	}
+	return (NULL);
+}
